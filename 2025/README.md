b345cf2ebf2c6aa3f6233a64dab6ca45e3bcbc61
Kacper Krej <kacper.krej@gmail.com>
2026-03-10 13:49:11 +0100

    2025 - day_10 part 2, but it's cheating

--- 2025/README.md
+++ 2025/README.md
@@ -1,3 +1,4 @@
 Uses:
 - Boost 1.83 - https://www.boost.org/
 - SafeInt 3.0.28a - https://github.com/dcleblanc/SafeInt
+- lp_solve 5.5.2.14 - https://github.com/lp-solve/lp_solve
--- 2025/day_10.cpp
+++ 2025/day_10.cpp
@@ -2,14 +2,17 @@
 #include <iostream>
 #include <vector>
 
+#include "lp_solve/lp_lib.h"
+
 // #define DEBUG
 
 #include "lib/utils.hpp"
 
+namespace part_one {
+
 struct Data {
   short target;                           // bitmask of target indicator lights
   std::vector<decltype(target)> buttons;  // bitmasks of button wirings
-  std::vector<short> joltage;
 };
 
 /*
@@ -48,7 +51,7 @@ std::vector<Data> parse(const std::vector<std::string>& input) {
     for (auto it = parsed.begin() + 1; it != parsed.end() - 1; ++it) {
       short wiring = 0;
       // remove brackets
-      std::string wiring_str = (*it).substr(1, (*it).size() - 2);
+      std::string wiring_str = it->substr(1, it->size() - 2);
       std::vector<std::string> wiring_parsed;
       boost::split(wiring_parsed, wiring_str, boost::is_any_of(","));
       for (auto i : wiring_parsed) {
@@ -56,25 +59,10 @@ std::vector<Data> parse(const std::vector<std::string>& input) {
       }
       buttons.push_back(wiring);
     }
-
-    // joltage
-    decltype(Data::joltage) joltage;
-    // remove brackets
-    std::string joltage_str =
-        (*(parsed.end() - 1)).substr(1, (*(parsed.end() - 1)).size() - 2);
-    std::vector<std::string> joltage_parsed;
-    boost::split(joltage_parsed, joltage_str, boost::is_any_of(","));
-    joltage.reserve(joltage_parsed.size());
-    for (auto i : joltage_parsed) {
-      joltage.push_back(std::stoi(i));
-    }
-
-    out.push_back({target, buttons, joltage});
+    out.push_back({target, buttons});
   }
   return out;
 }
-
-namespace part_one {
 
 struct Combination {
   // bitmask of combined buttons
@@ -91,7 +79,7 @@ struct Combination {
 
 int solution(const std::vector<Data>& input) {
   int sol = 0;
-  for (auto [target, buttons, _] : input) {
+  for (auto [target, buttons] : input) {
     // check every combination of buttons until result matches the target.
     // no multiple presses for any button (they cancel each other).
     // button pressing order doesn't matter.
@@ -141,6 +129,206 @@ int solution(const std::vector<Data>& input) {
 }
 }  // namespace part_one
 
+namespace part_two_cheating {
+
+struct Button {
+  std::vector<int> wiring_idxs;
+};
+
+struct Data {
+  std::vector<int> target;
+  std::vector<Button> buttons;
+};
+
+std::vector<Data> parse(const std::vector<std::string>& input) {
+  std::vector<Data> out;
+  out.reserve(input.size());
+  for (const auto& line : input) {
+    std::vector<std::string> parsed;
+    boost::split(parsed, line, boost::is_any_of(" "));
+
+    // target
+    decltype(Data::target) target;
+    // remove brackets
+    std::string target_str =
+        (parsed.end() - 1)->substr(1, (parsed.end() - 1)->size() - 2);
+    std::vector<std::string> target_parsed;
+    boost::split(target_parsed, target_str, boost::is_any_of(","));
+    target.reserve(target_parsed.size());
+    for (auto i : target_parsed) {
+      target.push_back(std::stoi(i));
+    }
+
+    // buttons
+    decltype(Data::buttons) buttons;
+    buttons.reserve((parsed.end() - 1) - (parsed.begin() + 1));
+    for (auto it = parsed.begin() + 1; it != parsed.end() - 1; ++it) {
+      Button button;
+      // remove brackets
+      std::string wiring_str = it->substr(1, it->size() - 2);
+      std::vector<std::string> wiring_parsed;
+      boost::split(wiring_parsed, wiring_str, boost::is_any_of(","));
+      button.wiring_idxs.reserve(wiring_parsed.size());
+      for (auto i : wiring_parsed) {
+        button.wiring_idxs.push_back(std::stoi(i));
+      }
+      buttons.push_back(button);
+    }
+
+    out.push_back({target, buttons});
+  }
+  return out;
+}
+
+int solution(const std::vector<Data>& input) {
+  /*
+    Solve minimum number of button presses with linear program solver
+    (lp_solver).
+
+    - variables are numbers of each button presses
+    - constraints are target joltage values
+
+    e.g. (for first input):
+    [.##.] (3) (1,3) (2) (2,3) (0,2) (0,1) {3,5,4,7}
+
+    There are 6 buttons - 6 variables
+
+    With button wirings and targets they make a linear problem:
+
+      minimize x1 + x2 + x3 + x4 + x5 + x6
+
+      with constraints:
+        (columns of
+        this matrix
+        are button
+        wirings)
+      | 0 0 0 0 1 1 |   | x1 |   | 3 |
+      | 0 1 0 0 0 1 | * | x2 | = | 5 |
+      | 0 0 1 1 1 0 |   | x3 |   | 4 |
+      | 1 1 0 1 0 0 |   | x4 |   | 7 |
+                        | x5 |
+                        | x6 |
+      and
+      (this goes by default in lp_solver)
+      | x1 |    | 0 |
+      | x2 |    | 0 |
+      | x3 | <= | 0 |
+      | x4 |    | 0 |
+      | x5 |    | 0 |
+      | x6 |    | 0 |
+
+    In lp_solver objective function (the minimization) and constraints
+    coefficients take the form of a single matrix:
+
+      | 0 1 1 1 1 1 1 |  <- objective
+      | 0 0 0 0 0 1 1 |  <- constraints
+      | 0 0 1 0 0 0 1 |
+      | 0 0 0 1 1 1 0 |
+      | 0 1 1 0 1 0 0 |
+        ^
+        additional column with 0 values (that's because lp_solver starts
+        indexing from 1)
+
+  */
+
+  int all_presses_num = 0;
+#ifdef DEBUG
+  int dbg_counter = 0;
+#endif
+  for (auto [target, buttons] : input) {
+#ifdef DEBUG
+    // this just prints data
+    std::cout << "#" << dbg_counter << ":" << std::endl;
+    for (const auto& button : buttons) {
+      std::cout << "(";
+      for (const auto& wiring : button.wiring_idxs) {
+        std::cout << wiring << ",";
+      }
+      std::cout << ") ";
+    }
+    std::cout << "{";
+    for (const auto& val : target) {
+      std::cout << val << ",";
+    }
+    std::cout << "}" << std::endl;
+#endif
+
+    // set up problem
+    lprec* problem;
+    int n_constraints = target.size();
+    int n_vars = buttons.size();
+    // rows are constraints
+    // columns correspond to variables
+    problem = make_lp(n_constraints, n_vars);
+    // this is a minimization problem
+    set_minim(problem);
+    // reduce solver logs
+    set_verbose(problem, IMPORTANT);
+
+    // set problem variables to integers
+    for (int i = 1; i <= n_vars; ++i) {
+      set_int(problem, i, true);
+    }
+
+    // set problem matrix by columns
+    // first column is all zeroes (for indexing from 1; they're set by default)
+    // next columns are objective function coefficient (always 1)
+    // and constraint functions coefficients (from button wirings)
+    int i = 1;
+    for (auto& button : buttons) {
+      // use sparse arrays - with array of non-zero coefficients and array
+      // of their indexes
+
+      // coefficients - array of ones with size equal to number of button
+      // wirings + 1 (for objective function)
+      std::vector<double> column(button.wiring_idxs.size() + 1, 1);
+
+      // indexes - objective function and button wirings
+      std::vector<int> rowno;
+      rowno.reserve(button.wiring_idxs.size() + 1);
+      // objective
+      rowno.push_back(0);
+      // button wirings - indexes increased by 1 (to account for objective)
+      for (const auto& idx : button.wiring_idxs) {
+        rowno.push_back(idx + 1);
+      }
+
+      set_columnex(problem, i, column.size(), column.data(), rowno.data());
+
+      ++i;
+    }
+    // set constraints type and right-hand side
+    for (i = 1; i <= n_constraints; ++i) {
+      set_constr_type(problem, i, EQ);
+      set_rh(problem, i, target[i - 1]);
+    }
+
+    // // use this to see model
+    // print_lp(problem);
+    
+    solve(problem);
+
+    // number of presses is the problem objective
+    all_presses_num += get_objective(problem);
+
+#ifdef DEBUG
+    std::cout << "objective: " << get_objective(problem) << std::endl;
+    // get solution variable values
+    double* sol;
+    get_ptr_variables(problem, &sol);
+    std::cout << "variables: ";
+    for (int i = 0; i < n_vars; ++i) {
+      std::cout << sol[i] << ", ";
+    }
+    std::cout << std::endl << std::endl;
+    ++dbg_counter;
+#endif
+  }
+
+  return all_presses_num;
+}
+}  // namespace part_two_cheating
+
 int main() {
   const std::string input_filename = "input.txt";
   // const std::string input_filename = "input_example.txt";
@@ -148,9 +336,11 @@ int main() {
 
   auto execution_timer = ExecutionTimer();
 
-  auto input = parse(read_file(input_filename));
-  auto result = part_one::solution(input);
-  // auto result = part_two::solution(input);
+  // auto input = part_one::parse(read_file(input_filename));
+  // auto result = part_one::solution(input);
+
+  auto input = part_two_cheating::parse(read_file(input_filename));
+  auto result = part_two_cheating::solution(input);
 
   auto execution_time_us = execution_timer.GetTimeUs();
 
